11340e046e6ee6228d672c9310725d9afa19a10e
betacoder7 <webdesigner2502@gmail.com>
2025-12-09 11:36:17 +0530

    package change

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/language-king-website-new",
-  plugins: [react(),tailwindcss()],
+  base: "/language-king/",   // repo name
+  plugins: [react(), tailwindcss()],
+  build: {
+    assetsDir: "", // short paths to FIX ENAMETOOLONG
+  }
 })
